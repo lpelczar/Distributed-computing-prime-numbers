7302e81b91053fefcb5d12fea8e3f9ad441aa4e4
mrezlik <mrezlik@gmail.com>
2018-06-26 14:59:11 +0200

    Implement simple algorithm to check if numbe is a prime

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,30 @@
+import java.math.BigInteger;
+
 public class App {
+    public static void main(String[] args) {
+        BigInteger maxValue = new BigInteger("1111111");
+        for(BigInteger value = new BigInteger("21323"); value.compareTo(maxValue) <= 0; value = value.add(new BigInteger("1"))){
+            if(isPrime(value)){
+                System.out.println("IT's PRIME!" + value);
+            };
+        }
+    }
+
+    private static boolean isPrime(BigInteger bigPrime){
+        if(bigPrime.compareTo(new BigInteger("1")) == 0 || bigPrime.compareTo(new BigInteger("2")) == 0 || bigPrime.compareTo(new BigInteger("3")) == 0){
+            return true;
+        }
+        if(BigInteger.ZERO.equals(bigPrime.mod(new BigInteger("2")))){
+            return false;
+        }
+
+        for(BigInteger i = new BigInteger("3"); i.multiply(i).compareTo(bigPrime) < 1; i = i.add(new BigInteger("2")) ){
+            if(BigInteger.ZERO.equals(bigPrime.mod(i))){
+
+                return  false;
+            }
+        }
+
+        return true;
+    }
 }
