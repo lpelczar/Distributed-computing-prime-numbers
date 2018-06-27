59fec10bbe32a4d576269715120ec3302e39e3ad
lpelczar <lukepelczar@gmail.com>
2018-06-27 13:08:23 +0200

    Add integer checker util class

---
+++ src/main/java/utils/IntegerChecker.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class IntegerChecker {
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException | NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+}
