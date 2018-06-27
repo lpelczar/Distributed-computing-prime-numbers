c599ea19f1a5812fb67a31d83e9dffe3ced5bab2
lpelczar <lukepelczar@gmail.com>
2018-06-27 10:57:48 +0200

    Fix range class

--- src/main/java/models/Range.java
+++ src/main/java/models/Range.java
@@ -10,25 +10,34 @@ public class Range {
     private BigInteger max;
 
     public Range(BigInteger min, BigInteger max) {
+        if (min.compareTo(max) >= 1) {
+            throw new IllegalArgumentException("Min cannot be greater than max!");
+        }
+
         this.min = min;
         this.max = max;
     }
 
-    public List<Range> divideRanges(BigInteger number) {
+    public List<Range> divideRanges(Integer number) {
 
         BigInteger fullRange = max.subtract(min);
-        BigInteger dividedRange = fullRange.divide(number);
+        BigInteger dividedRange = fullRange.divide(BigInteger.valueOf(number));
 
         List<Range> ranges = new ArrayList<>();
 
         BigInteger minValue = min;
-        while (minValue.compareTo(max) <= -1) {
-            Range range = new Range(minValue, minValue = minValue.add(dividedRange));
+
+        for (int i = 1; i <= number; i++) {
+            if (i != 1) {
+                minValue = minValue.add(new BigInteger("1"));
+            }
+
+            Range range = new Range(minValue, minValue = minValue.add(dividedRange).subtract(new BigInteger("1")));
             ranges.add(range);
         }
 
         if (minValue.compareTo(max) < 0) {
-            ranges.get(ranges.size()).setMax(max);
+            ranges.get(ranges.size() - 1).setMax(max);
         }
 
         return ranges;
