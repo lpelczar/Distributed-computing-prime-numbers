c21e4826dcd69431a0a2ee46e6300527c99804a3
martastaporek <staporekmarta1@gmail.com>
2018-06-28 11:22:41 +0200

    Implement test for short ranges with small values

---
+++ src/test/java/solver/PrimeCalculatorTest.java
@@ -0,0 +1,36 @@
+package solver;
+
+import models.Range;
+import models.Result;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeCalculatorTest {
+
+    private PrimeCalculator primeCalculator;
+
+    @Test
+    void test_OneIsNotPrime(){
+        Range range = new Range(BigInteger.ZERO, BigInteger.ONE);
+        this.primeCalculator = new PrimeCalculator(range);
+
+        Result testResult = this.primeCalculator.calculate();
+        assertEquals(testResult.getNumbers().size(), 0);
+
+    }
+
+    @Test
+    void test_TenFirstPrimes(){
+        Range range = new Range(BigInteger.ZERO, BigInteger.TEN);
+        this.primeCalculator = new PrimeCalculator(range);
+        Result testResult = this.primeCalculator.calculate();
+        assertEquals(testResult.toString(), "[2, 3, 5, 7]");
+    }
+
+}
