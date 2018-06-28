86e52027a9d59afa7c4d01b92991c352e34c8c26
martastaporek <staporekmarta1@gmail.com>
2018-06-28 11:46:56 +0200

    Implement test for range without prime numbers

--- src/test/java/solver/PrimeCalculatorTest.java
+++ src/test/java/solver/PrimeCalculatorTest.java
@@ -2,12 +2,9 @@ package solver;
 
 import models.Range;
 import models.Result;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
-import java.util.Set;
-import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +18,7 @@ class PrimeCalculatorTest {
         this.primeCalculator = new PrimeCalculator(range);
 
         Result testResult = this.primeCalculator.calculate();
-        assertEquals(testResult.getNumbers().size(), 0);
+        assertEquals(0, testResult.getNumbers().size());
 
     }
 
@@ -30,7 +27,7 @@ class PrimeCalculatorTest {
         Range range = new Range(BigInteger.ZERO, BigInteger.TEN);
         this.primeCalculator = new PrimeCalculator(range);
         Result testResult = this.primeCalculator.calculate();
-        assertEquals(testResult.toString(), "[2, 3, 5, 7]");
+        assertEquals("[2, 3, 5, 7]", testResult.toString());
     }
 
     @Test
@@ -38,7 +35,16 @@ class PrimeCalculatorTest {
         Range range = new Range(new BigInteger("-100"), BigInteger.ONE);
         this.primeCalculator = new PrimeCalculator(range);
         Result testResult = this.primeCalculator.calculate();
-        assertEquals(testResult.getNumbers().size(), 0);
+        assertEquals(0, testResult.getNumbers().size());
     }
+
+    @Test
+    void test_RangeWithoutPrimes(){
+        Range range = new Range(new BigInteger("104724"), new BigInteger("104728"));
+        this.primeCalculator = new PrimeCalculator(range);
+        Result testresult = this.primeCalculator.calculate();
+        assertEquals(0, testresult.getNumbers().size());
+    }
+    
 
 }
